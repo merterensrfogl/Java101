776d2ea9848e06edc4d1b6aa77d11d92eb7d651d
Mert Eren Serifoglu <ernsrfogl@hotmail.com>
2023-05-06 16:54:03 +0300

    Burç Bulan Program

---
+++ burcProgrami.java
@@ -0,0 +1,58 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class burcProgrami
+{
+    public static void main(String[] args)
+    {
+        int ay;
+        int gun;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Doğduğunuz ayı sayıyle giriniz: ");
+        ay = input.nextInt();
+
+        System.out.print("Doğduğunuz günü giriniz: ");
+        gun = input.nextInt();
+
+        if ((ay == 1 && gun >= 22) || (ay == 2 && gun <= 19))
+            System.out.print("Burcunuz Kova");
+
+        else if ((ay == 2 && gun >= 20) || (ay == 3 && gun <= 20))
+            System.out.print("Burcunuz Balık");
+
+        else if ((ay == 3 && gun >= 21) || (ay == 4 && gun <= 20))
+            System.out.print("Burcunuz Koç");
+
+        else if ((ay == 4 && gun >= 21) || (ay == 5 && gun <= 21))
+            System.out.print("Burcunuz Boğa");
+
+        else if ((ay == 5 && gun >= 22) || (ay == 6 && gun <= 22))
+            System.out.print("Burcunuz İkizler");
+
+        else if ((ay == 6 && gun >= 23) || (ay == 7 && gun <= 22))
+            System.out.print("Burcunuz Yengeç");
+
+        else if ((ay == 7 && gun >= 23) || (ay == 8 && gun <= 22))
+            System.out.print("Burcunuz Aslan");
+
+        else if ((ay == 8 && gun >= 23) || (ay == 9 && gun <= 22))
+            System.out.print("Burcunuz Başak");
+
+        else if ((ay == 9 && gun >= 23) || (ay == 10 && gun <= 22))
+            System.out.print("Burcunuz Terazi");
+
+        else if ((ay == 10 && gun >= 23) || (ay == 11 && gun <= 21))
+            System.out.print("Burcunuz Akrep");
+
+        else if ((ay == 11 && gun >= 22) || (ay == 12 && gun <= 21))
+            System.out.print("Burcunuz Yay");
+
+        else if ((ay == 12 && gun >= 22) || (ay == 1 && gun <= 21))
+            System.out.print("Burcunuz Oğlak");
+        else
+            System.out.println("Geçersiz Giriş Yaptınız!");
+
+    }
+}
